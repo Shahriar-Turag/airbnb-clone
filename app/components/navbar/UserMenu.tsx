fbffe8376c1497053f4d759d092ff2bcc8d27970
Shahriar Turag <shahriarturag.st@gmail.com>
2023-05-10 17:03:50 +0600

    Add lists and favorites

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -32,6 +32,8 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 		rentModal.onOpen();
 	}, [currentUser, loginModal, rentModal]);
 
+	// console.log(currentUser);
+
 	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
